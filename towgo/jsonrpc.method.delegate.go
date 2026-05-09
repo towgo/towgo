7a09d923b7d547df35b9681712153db1afb18a65
Rice <1840169763@qq.com>
2026-05-09 17:03:36 +0800

    v2.0

--- towgo/jsonrpc.method.delegate.go
+++ towgo/jsonrpc.method.delegate.go
@@ -544,7 +544,7 @@ func doParse(conn JsonRpcConnection, fields []gstructs.Field, pointer interface{
 			Data(pointer).
 			Assoc(data).
 			Run(conn.Context()); err != nil {
-			return gerror.Wrap(err, `failed to validate pointer`)
+			return gerror.Wrapf(err, `failed to validate pointer %+v,data %+v`, pointer, data)
 		}
 
 	// Multiple struct, it only supports JSON type post content like:
@@ -569,7 +569,7 @@ func doParse(conn JsonRpcConnection, fields []gstructs.Field, pointer interface{
 				Data(reflectVal2.Index(i)).
 				Assoc(j.Get(gconv.String(i)).Map()).
 				Run(conn.Context()); err != nil {
-				return gerror.Wrap(err, `failed to validate pointer`)
+				return gerror.Wrapf(err, `failed to validate pointer %+v,data %+v`, reflectVal2.Index(i), j.Get(gconv.String(i)).Map())
 			}
 		}
 	}
